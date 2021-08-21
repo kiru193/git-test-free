223640d08bc4bd3a623395e537cc76849b8c1c2c
kiru193 <program922@hotmail.com>
2021-08-21 23:25:01 +0900

    ブランチを作って機能の追加

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
 int main() {
+	int m=true;
+
 	printf("hello world!");
 	for (int i = 0; i < 100; i++) {
 		printf("%d\n", i);
 		if (i < 60 && i>50) {
-			printf("%d", rand();
+			printf("%d", rand());
 		}
+	}
+
+	if (m == true) {
+		printf("true\n");
+	}
+	else {
+		printf("false\n");
 	}
 }
