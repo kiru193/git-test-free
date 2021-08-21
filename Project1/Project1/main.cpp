9ee1df1f1535b0120e1e8e64e9256c6afebb00f2
DESKTOP-FMII11R\zi-ku <program922@hotmail.com>
2021-08-21 23:28:45 +0900

    デスクトップでもブランチを切って機能の追加

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -5,7 +5,11 @@ int main() {
 	for (int i = 0; i < 100; i++) {
 		printf("%d\n", i);
 		if (i < 60 && i>50) {
-			printf("%d", rand();
+			printf("%d", rand());
 		}
+	}
+
+	for (int m = 0; m < 100; m += 4) {
+		printf("%d\n", m);
 	}
 }
